69ecb810bb038b1e0a7cca2be59f3c95e0e6f4e6
d33pster <deep.main.ac@gmail.com>
2023-10-01 18:37:13 +0530

    buildme

--- buildme.cmd
+++ buildme.cmd
@@ -1,13 +1,12 @@
 @echo off
-runas /user:Administrator cmd
 pip install pyinstaller
 mkdir process-plot-win
 pyinstaller --onefile setup --uac-admin -i main\pp.ico
 del setup.spec
-rmdir build /s /q
+runas /user:Administrator rmdir build /s /q
 cd dist
 copy ./setup.exe ../setup.exe
 cd ..
-rmdir dist /s /q
-copy setup.exe process-plot-win\setup.exe
+runas /user:Administrator rmdir dist /s /q
+copy setup.exe process-plot-win/setup.exe
 del setup.exe
