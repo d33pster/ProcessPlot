2f101f8e8cd67f3f299a7a3fbe52776c9e710357
d33pster <deep.main.ac@gmail.com>
2023-10-01 18:15:15 +0530

    buildme

--- buildme.cmd
+++ buildme.cmd
@@ -2,3 +2,11 @@
 pip install pyinstaller
 mkdir process-plot-win
 pyinstaller --onefile setup --uac-admin -i main\pp.ico
+rm setup.spec
+rm -r build
+cd dist
+copy ./setup.exe ../setup.exe
+cd ..
+rm -r dist
+copy setup.exe process-plot-win\setup.exe
+rm setup.exe
